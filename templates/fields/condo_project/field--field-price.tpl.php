5f7c8e384360edd8ea041d2cbd84a9767e3770fe
Jake Ketcheson <snowcave@gmail.com>
2016-01-27 16:01:04 -0800

    added in field price check if its set to 0

---
+++ templates/fields/condo_project/field--field-price.tpl.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @file field--fences-div.tpl.php
+ * Wrap each field value in the <div> element.
+ *
+ * @see http://developers.whatwg.org/grouping-content.html#the-div-element
+ */
+if(render($items[0]) === '$0'){
+  $label = '';
+}
+?>
+<div class="field-container <?php print $classes; ?>">
+  <div class="inner">
+<?php if ($element['#label_display'] == 'inline'): ?>
+  <span class="field-label"<?php print $title_attributes; ?>>
+    <?php print $label; ?>
+  </span>
+<?php elseif ($element['#label_display'] == 'above'): ?>
+  <h4 class="field-label"<?php print $title_attributes; ?>>
+    <?php print $label; ?>
+  </h4>
+<?php endif; ?>
+
+<?php foreach ($items as $delta => $item): ?>
+
+  <div class="field-item"<?php print $attributes; ?>>
+    <?php if(render($item) !== '$0'):?>
+      <?php print render($item); ?>
+    <?php else:?>
+      Price coming soon
+    <?php endif;?>
+  </div>
+<?php endforeach; ?>
+  </div>
+</div>
